49f1836f2d150d7ea961463332600192c15bb5fe
giorgi18806 <giorgi18806@gmail.com>
2023-10-13 02:26:42 +0300

    Finish stage 1

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome123');
+    return view('welcome');
 })->name('home');
 
 Route::get('/login', function () {
